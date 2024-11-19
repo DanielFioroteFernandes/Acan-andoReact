fedf13791945778cc8029f345e9f47ceb484faec
danielfiorotefernandes <danielfiorote@hotmail.com>
2024-11-18 22:36:58 -0300

    cadastrando post

--- 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
+++ 10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -1,9 +1,103 @@
 import styles from "./CreatePost.module.css";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    //validar URL da image
+
+    //Criar Array de tags
+
+    //checar todos os valores
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to home page
+  };
+
   return (
-    <div>
-      <h1>CreatePost</h1>
+    <div className={styles.create_post}>
+      <h2>Criar Post</h2>
+      <p>
+        Escreva sobre o que você quiser, menos do Xandão e compartilhe seu
+        conhecimento.
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense em um bom título..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>URL da Imagem</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Ensira uma imagem que represente seu post..."
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Pense em um bom conteúdo..."
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Ensira as tags separado por vírgula"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+
+        {!response.loading && <button className="btn">Criar Post</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {response.error && <p className="error"> {response.error}</p>}
+      </form>
     </div>
   );
 };
